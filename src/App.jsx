904be7be882ce2e66712346b1b711e1ea733a685
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-09-03 02:12:47 +0100

    Update many item to navigate using useNavigate hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import {Routes, Route} from 'react-router-dom'
 import HomePage from "./pages/homepage/HomePage";
+
+const HatsPage = (props)=>{
+  console.log(props)
+  return <div><h1>HATS PAGE</h1></div>
+}
+
 function App() {
   return (
     <main>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop/hats' element={<HatsPage />} />
+      </Routes>
     </main>
   );
 }
--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,8 +1,10 @@
+import {useNavigate} from 'react-router-dom'
 import "./menu-item.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate()
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
       <div
         className="background-image"
         style={{
