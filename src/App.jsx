738be603ee348ed96d6b8142e64cfd209079149f
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-09-04 23:03:04 +0100

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
+import Header from './components/header/Header'
 import './App.css'
 function App() {
   return (
     <main>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import './header.scss'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/tousLogo.png'
+const Header = () => {
+  return (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <img className='logo' src={logo} alt='Logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/contact'>
+          CONTACT
+        </Link>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header
