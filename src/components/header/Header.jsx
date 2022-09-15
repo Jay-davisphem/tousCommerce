8b0751a013cb541d13a00301dbc6f8b84ae4d531
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-09-15 13:51:49 +0100

    Revert back to previous

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,8 @@
 import "./header.scss";
 import { Link } from "react-router-dom";
 import logo from "../../assets/tousLogo.png";
-import { setCurrentUser } from './redux/user/userSlice
 import { auth } from "../../firebase/firebase.utils";
 const Header = ({ currentUser }) => {
-  const currentUser = useSelector(state => state.currentUser)
-  const dispatch = useDispatch()
   return (
     <div className="header">
       <Link className="logo-container" to="/">
