baf6992e82d36f8c8ed9b652a484723bf474595d
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-09-17 16:35:39 +0100

    Rendering of cart counts at every state change

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,15 +1,21 @@
 import "./cart-icon.scss";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import imgSrc from "../../assets/shopping-bag.svg";
 import { cartActions } from "../../redux";
 const CartIcon = () => {
   const { toggleCartHidden } = cartActions;
   const dispatch = useDispatch();
+  const itemCount = useSelector(state => {
+    console.error('I am being called!')
+    return state.cart.cartItems
+      .reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+}
+)
 
   return (
     <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
       <img src={imgSrc} className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
