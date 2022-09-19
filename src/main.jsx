eaba5b6b9c88da9cfa1b97533ccc17f5ba0e4e7e
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-09-19 04:42:10 +0100

    Add persistence to cart using redux-persist localstorage

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import { store } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
+import store from "./redux/store";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <Provider store={store.store}>
     <Router>
+      <PersistGate persistor={store.persistor}>
       <App />
+      </PersistGate>
     </Router>
   </Provider>
 );
