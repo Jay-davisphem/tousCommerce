bfdb8512f49358e18c6d197df35d7ebc86d6ed60
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-09-22 07:43:47 +0100

    Still on routing

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -1,16 +1,18 @@
+import {useMemo} from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import './collection.scss'
 import CollectionItem from '../../components/collection-item/CollectionItem'
 import {selectCollection} from '../../redux/shop/shop.selectors'
-const CollectionPage = () => {
-  const params = useParams()
-  const collection = useSelector(state => selectCollection(params.collectionId)(state))
 
+const CollectionPage = () => {
+  const collectionId = useParams().collectionId
+  const memoizedSelect = useMemo(() => selectCollection(collectionId), [collectionId])
+  const collection = useSelector(state => memoizedSelect(state))
   console.log(collection)
   return (
     <div className='collection-page'>
-      <h2>COLLECTION PAGE</h2>
+      <h2>{collectionId.toUpperCase()}</h2>
     </div>
   )
 }
