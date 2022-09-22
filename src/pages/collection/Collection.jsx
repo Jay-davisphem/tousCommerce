854fe2818074f884a5084a4977e72828305172b9
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-09-22 07:18:55 +0100

    Add selector to route each shop collections

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -1,11 +1,13 @@
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import './collection.scss'
-
 import CollectionItem from '../../components/collection-item/CollectionItem'
-
+import {selectCollection} from '../../redux/shop/shop.selectors'
 const CollectionPage = () => {
   const params = useParams()
-  console.log(params)
+  const collection = useSelector(state => selectCollection(params.collectionId)(state))
+
+  console.log(collection)
   return (
     <div className='collection-page'>
       <h2>COLLECTION PAGE</h2>
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,25 @@
 import { createSelector } from "reselect";
 
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+  childrens: 6,
+  students: 7,
+  gadgets: 8,
+  homes: 9,
+  pets: 10,
+};
+
 const shopSelector = (state) => state.shop;
-console.log("IN SELECTORS");
 export const shopCollectionsSelector = createSelector(
   [shopSelector],
   (shop) => shop.collections
 );
+
+export const selectCollection = (param) =>
+  createSelector([shopCollectionsSelector], (collections) =>
+    collections.find((collection) => collection.id === COLLECTION_ID_MAP[param])
+  );
