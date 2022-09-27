5a2c661980bcf4a2317e0038f7cf00a8a96651c3
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-09-27 18:15:21 +0100

    Push in case system crash totally

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,6 +1,18 @@
-import CollectionOverview from "../../components/collection-overview/CollectionOverview";
+import {useEffect} from 'react'
 
+import CollectionOverview from "../../components/collection-overview/CollectionOverview";
+import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils'
 const ShopPage = () => {
+  const unsubscritbeFromSnapshot = null
+  useEffect(() => {
+    const collectionRef = firestore.collection('collections')
+
+
+    collectionRef.onSnapshot(async snapshot => {
+      const convertCollectionsSnapshotToMap(snapshot)
+    })
+  }, [])
+
   console.log(location, "my history");
   return (
     <div className="shop-page">
